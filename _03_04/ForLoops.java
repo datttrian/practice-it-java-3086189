f8c7c73a4c8be0ff68a11dd32d0a269faaefdc35
Dat T. Tran <datttrian@gmail.com>
2023-07-20 08:32:38 +0000

    For loops

--- _03_04/ForLoops.java
+++ _03_04/ForLoops.java
@@ -4,12 +4,16 @@ public class ForLoops {
 
   public static void main(String[] args) {
     // Write a for loop that prints out the phrase "I love for loops" 5 times
-
+    for (int i = 0; i < args.length; i++) {
+      System.out.println("I love for loops.");
+    }
     // Write a for loop that prints out the numbers 1 to 10
-
+    for (int i = 0; i < 11; i++) {
+      System.out.println(i);
+    }
     // Write a for loop that prints out the numbers 10 to 1
-    
-
+    for (int i = 10; i > 0; i--) {
+      System.out.println(i);
+    }
   }
-
 }
