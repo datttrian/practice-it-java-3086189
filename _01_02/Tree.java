14e10973223068b5398a04980b0bec2d0eb51ee4
Dat T. Tran <datttrian@gmail.com>
2023-07-20 11:16:49 +0000

    Building objects with a constructor

--- _01_02/Tree.java
+++ _01_02/Tree.java
@@ -5,6 +5,12 @@ public class Tree {
   double trunkDiameterInches;
   TreeType treeType;
 
+  public Tree(double heightFt, double trunkDiameterInches, TreeType treeType) {
+    this.heightFt = heightFt;
+    this.trunkDiameterInches = trunkDiameterInches;
+    this.treeType = treeType;
+  }
+
   void grow() {
     this.heightFt = this.heightFt + 10;
     this.trunkDiameterInches = this.trunkDiameterInches + 1;
