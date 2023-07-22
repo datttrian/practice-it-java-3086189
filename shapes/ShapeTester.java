2ef705c9adbfa536fa9eca53fa87d466741a7160
Dat T. Tran <datttrian@gmail.com>
2023-07-22 09:25:40 +0000

    Creating objects with abstract types

---
+++ shapes/ShapeTester.java
@@ -0,0 +1,8 @@
+package shapes;
+
+public class ShapeTester {
+  public static void main(String[] args) {
+    Shape rectangle = new Rectangle(5, 7);
+    System.out.println(rectangle.calculateArea());
+  }
+}
