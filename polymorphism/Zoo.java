da50ee1445accdc216b06a7b8cec8bd7c1e7d333
Dat T. Tran <datttrian@gmail.com>
2023-07-22 08:42:44 +0000

    instanceof operator

--- polymorphism/Zoo.java
+++ polymorphism/Zoo.java
@@ -6,16 +6,38 @@ public class Zoo {
     Dog rocky = new Dog();
     rocky.fetch();
     rocky.makeSound();
+    feed(rocky);
 
     Animal sasha = new Dog();
-    sasha.makeSound();
 
-    Dog sashaTheDog = (Dog) sasha;
-    sashaTheDog.fetch();
+    if (sasha instanceof Dog sashaTheDog) {
+      sashaTheDog.fetch();
+    }
+
+    if (sasha instanceof Dog) {
+      Dog sashaTheDog = (Dog) sasha;
+      sashaTheDog.fetch();
+    }
 
     sasha = new Cat();
     sasha.makeSound();
+    feed(sasha);
     ((Cat) sasha).scratch();
 
+    boolean isDog = sasha instanceof Dog;
+    boolean isAnimal = sasha instanceof Animal;
+    boolean isCat = sasha instanceof Cat;
+    System.out.println(isDog);
+    System.out.println(isAnimal);
+    System.out.println(isCat);
+
+  }
+
+  public static void feed(Animal animal) {
+    if (animal instanceof Dog) {
+      System.out.println("Here's your dog food");
+    } else {
+      System.out.println("Here's your cat food");
+    }
   }
 }
