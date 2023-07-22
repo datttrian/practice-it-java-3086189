108fb0b1a83aa0143f4702ad8d2e85fab952be8c
Dat T. Tran <datttrian@gmail.com>
2023-07-22 07:58:25 +0000

    Chain of inheritance

---
+++ vehicles/Coupe.java
@@ -0,0 +1,8 @@
+package vehicles;
+
+public class Coupe extends Car {
+
+  public Coupe() {
+    setDoors(2);
+  }
+}
---
+++ vehicles/Garage.java
@@ -0,0 +1,13 @@
+package vehicles;
+
+public class Garage {
+
+  public static void main(String[] args) {
+    Coupe myCar = new Coupe();
+    myCar.setColor("red");
+
+    System.out.println(
+        String.format("My car is %s and has %d doors.",
+            myCar.getColor(), myCar.getDoors()));
+  }
+}
